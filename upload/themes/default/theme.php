fe33e5f44ac627be7b0f6578bea5c27787678560
Qexy.org <qexyorg@users.noreply.github.com>
2020-03-02 23:49:51 +0200

    change site url

--- upload/themes/default/theme.php
+++ upload/themes/default/theme.php
@@ -5,7 +5,7 @@ if(!defined("MCR")){ exit("Hacking Attempt!"); }
 $theme = array(
 	"ThemeName" => "Default",
 	"Author" => "NC22; KillerProst; Toster_tpl",
-	"AuthorUrl" => "http://webmcr.com",
+	"AuthorUrl" => "https://webmcr.ru",
 	"About" => "Базовая тема, распоространяется по лицензии GPLv3",
 	"Version" => "3.0"
 );
